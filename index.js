a955d3b6b154bd62b35fab2e1ed480674c6a286d
saiful2612 <bristykodom@gmail.com>
2021-11-19 12:12:58 +0600

    env changed

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(cors());
 const PORT = process.env.PORT || 8000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mhs2r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mhs2r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
